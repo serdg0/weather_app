a87c53fab7203e29b6dc10acfa9001d76965845c
serdg0 <sergio.diazg92@gmail.com>
2020-01-13 14:06:00 -0600

    lint lint lint

--- src/dom.js
+++ src/dom.js
@@ -8,28 +8,31 @@ async function output(weatherData) {
   document.getElementById('celsius').innerHTML = `Celsius: ${(kelvin - 273.15).toFixed(1)}`;
   document.getElementById('fahrenheit').innerHTML = `Fahrenheit: ${((kelvin - 273.15) * 9 / 5 + 32).toFixed(1)}`;
   document.getElementById('description').innerHTML = `Status: ${weatherData.weather[0].description}`;
-};
+}
+
+async function button() {
+  const key = process.env.GIF_KEY;
+  const cityField = document.getElementById('city');
+  const city = cityField.value.toLowerCase();
+  const weatherData = await getWeather(city);
+  if (weatherData.message === undefined) {
+    const weatherName = weatherData.weather[0].main;
+    await output(weatherData);
+    await weatherGiphy(weatherName);
+  } else {
+    const errorGif = await fetch(`https://api.giphy.com/v1/gifs/HKmW8g1pG0Rig?api_key=${key}`);
+    const errorData = await errorGif.json();
+    document.getElementById('weather').src = errorData.data.images.original.url;
+    document.getElementById('celsius').innerHTML = 'FAILED TASK BRO/SIS';
+    document.getElementById('description').innerHTML = '';
+    document.getElementById('fahrenheit').innerHTML = '';
+  }
+}
 
 async function init() {
   const key = process.env.GIF_KEY;
-  const cityField = document.getElementById('city');
   const submit = document.getElementById('submit');
-  submit.onclick = async function () {
-    const city = cityField.value.toLowerCase();
-    const weatherData = await getWeather(city);
-    if (weatherData.message === undefined) {
-      const weatherName = weatherData.weather[0].main;
-      await output(weatherData);
-      await weatherGiphy(weatherName);
-    } else {
-      const errorGif = await fetch(`https://api.giphy.com/v1/gifs/HKmW8g1pG0Rig?api_key=${key}`);
-      const errorData = await errorGif.json();
-      document.getElementById('weather').src = errorData.data.images.original.url;
-      document.getElementById('celsius').innerHTML = 'FAILED TASK BRO/SIS';
-      document.getElementById('description').innerHTML = '';
-      document.getElementById('fahrenheit').innerHTML = '';
-    }
-  };
+  submit.onclick = () => button();
   const welcome = await fetch(`https://api.giphy.com/v1/gifs/ASd0Ukj0y3qMM?api_key=${key}`);
   const welcomeData = await welcome.json();
   document.getElementById('weather').src = welcomeData.data.images.original.url;
