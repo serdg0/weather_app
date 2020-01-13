0cca375afd8ff1ce7ba7029bbae305f4b6d2367a
serdg0 <sergio.diazg92@gmail.com>
2020-01-13 13:21:53 -0600

    Lints

--- webpack.config.js
+++ webpack.config.js
@@ -17,21 +17,21 @@ module.exports = {
         use: [
           'style-loader',
           'css-loader',
-        ]
+        ],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
           'file-loader',
-        ]
+        ],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
           'file-loader',
-        ]
-      }
-    ]
+        ],
+      },
+    ],
   },
   plugins: [
     new Dotenv()
